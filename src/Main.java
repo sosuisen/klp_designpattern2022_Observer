615032b505f836d649850b80d8e00f8823a4ba0a
sosuisen <hidekazu.kubota@gmail.com>
2023-01-22 23:33:24 +0900

    feat: add SumObserver

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
         var generator = new RandomNumberGenerator();
         var observer1 = new DigitObserver();
         var observer2 = new GraphObserver();
+        var observer3 = new SumObserver();
         generator.addObserver(observer1);
         generator.addObserver(observer2);
+        generator.addObserver(observer3);
         generator.execute();
     }
 }
